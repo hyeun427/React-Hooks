5d6c51140270bce2e8e8186649667ae9435ce44d
hyeun427 <80902406+hyeun427@users.noreply.github.com>
2022-08-09 17:46:24 +0900

    useMemo

---
+++ pages/useMemo/index.js
@@ -0,0 +1,11 @@
+import UseMemoCompo1 from "../../src/5.useMemo/1";
+import UseMemoCompo2 from "../../src/5.useMemo/2";
+
+export default function UseMemoPage() {
+  return (
+    <>
+      <UseMemoCompo1 />
+      <UseMemoCompo2 />
+    </>
+  );
+}
---
+++ src/5.useMemo/2/index.js
@@ -0,0 +1,31 @@
+import { useEffect, useMemo, useState } from "react";
+
+export default function UseMemoCompo2() {
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
+
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+
+  useEffect(() => {
+    console.log("useEffect 호출");
+    // 뭔가 오래 걸리는 작업..
+  }, [location]);
+
+  return (
+    <div>
+      <h2>하루에 몇끼 먹어요?</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라 : {location.country}</p>
+      <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>
+    </div>
+  );
+}
