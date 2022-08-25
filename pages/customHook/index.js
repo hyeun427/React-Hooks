f30a246cb6a0b6dc1265b0ad8d0b22ef6edd8ad2
hyeun427 <80902406+hyeun427@users.noreply.github.com>
2022-08-25 22:55:28 +0900

    custom hook

--- pages/customHook/index.js
+++ pages/customHook/index.js
@@ -3,9 +3,9 @@ import CustomHookCompo2 from "../../src/9.customHook/2";
 
 export default function CustomHookPage() {
   return (
-    <>
+    <div>
       <CustomHookCompo1 />
       <CustomHookCompo2 />
-    </>
+    </div>
   );
 }
